f5306692b9b6487c8fb6f7242d12e540608b365b
Malz53 <johnmalunga53@gmail.com>
2026-08-19 21:22:38 +0200

    Added some simple login test for Client app and captured data from the dashboard

---
+++ tests/ClientApp.spec copy.js
@@ -0,0 +1,22 @@
+const {test, expect} = require('@playwright/test');
+
+
+test.only('assignment1', async ({page})=>{
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    console.log(await page.title());
+    const cardTitles = page.locator(".card-body b");
+    const userEmail = page.locator('#userEmail');
+    const password = page.locator('#userPassword');
+    const signInBtn = page.locator('#login');
+
+    await userEmail.fill("mare@gmail.com");
+    await password.fill("M@123456m");
+    await signInBtn.click();
+    await cardTitles.first().waitFor();
+    //or you can use await page.waitForLoadState('networkidle');
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+})
+
+
+
--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,19 +1,41 @@
 const {test, expect} = require('@playwright/test');
 
 
-test.only('Browser Context Playwright Test', async ({browser})=>{
- const context = await browser.newContext();
+test('Browser Context Playwright Test', async ({browser})=>{
+    
+
+    const context = await browser.newContext();
     const page = await context.newPage();
+
+    const userName = page.locator('#username');
+    const signInBtn = page.locator('#signInBtn');
+    const password = page.locator("[type='password']");
+    const cardTitles = page.locator(".card-body a");
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
     //css, xpath, fill can be used to locate elements
-    await page.locator('#username').fill("Malz");
-    await page.locator("[type='password']").fill("123456");
-    await page.locator('#signInBtn').click();
+    await userName.fill("Malz");
+    await password.fill("Learn833mK2)");
+    await signInBtn.click();
     //wait until this locator shows up in DOM
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
+    await userName.fill("");
+    await password.fill("");
+    await userName.fill("rahulshettyacademy");
+    await password.fill("Learning@830$3mK2"); 
+    await signInBtn.click();
+    //console.log(await page.locator(".card-body a").first().textContent());
+    //console.log(await cardTitles.nth(0).textContent());
+    //allTextContent is not subject to auto-wait
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
+
 })
+
+
 
 test('first playwright test', async ({page})=>{
     
